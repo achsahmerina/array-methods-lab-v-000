f54de67dcea1a85d150a8f34680a82324c1d57f7
Achsah Abraham <achsahmp@gmail.com>
2018-08-02 23:55:51 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -23,6 +23,6 @@ def using_last(array)
 end
 
 def using_size(array)
-  array_numbers = ["wow", "dog", "arrays!", 6]
+  array_numbers = ["wow", "dog", "arrays!", "window", "code", "computer"]
   array_numbers.last
 end
