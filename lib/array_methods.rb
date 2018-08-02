240c9752e71dbdbd60e8d19a920acc1bda03678f
Achsah Abraham <achsahmp@gmail.com>
2018-08-02 23:48:46 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,7 +5,7 @@ end
 def using_sort(array)
   array_numbers = ["10", "5", "11", "2"]
   array_numbers.sort
-    =>["2", "5", "10", "11"]
+    => array_numbers = ["2", "5", "10", "11"]
 end
 
 def using_reverse(array)
